1bbe13509b3e4eef72ec064ff70dd7e7989258e1
Ryan Diel <rdiel01@gmail.com>
2018-12-03 19:05:16 -0600

    add all required tasks

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -19,9 +19,43 @@ public class SearchController {
     @RequestMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("searchType", "all");
         return "search";
     }
 
     // TODO #1 - Create handler to process search request and display results
-
+    @RequestMapping(value = "results")
+    public String search(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        /*
+                return all if all is SearchType and no searchterm was entered
+                 */
+        if (searchTerm.isEmpty() && searchType.contentEquals("all")) {
+                ArrayList<HashMap<String, String>> jobs = JobData.findAll();
+                model.addAttribute("columns", ListController.columnChoices);
+                model.addAttribute("title", "All jobs");
+                model.addAttribute("searchType", searchType);
+                model.addAttribute("jobs", jobs);
+                return "search";
+                /*
+                This should search all rows and return anything with matching search term
+                 */
+            } else if (searchType.contentEquals("all")) {
+                ArrayList<String> jobs = JobData.findAll(searchTerm);
+                model.addAttribute("columns", ListController.columnChoices);
+                model.addAttribute("title", "All jobs with " + searchTerm);
+                model.addAttribute("searchType", searchType);
+                model.addAttribute("jobs", jobs);
+                return "search";
+                /*
+                return  all rows that match searchterm and searchtype
+                 */
+            } else {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("columns", ListController.columnChoices);
+            model.addAttribute("title", "Showing "+searchType+" with "+searchTerm);
+            model.addAttribute("searchType", searchType);
+            model.addAttribute("jobs", jobs);
+            return "search";
+        }
+    }
 }
